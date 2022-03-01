96f02d19a8040a9e4fea6675dac5c2fd7c8aa9a3
Shoyur <100218550+Shoyur@users.noreply.github.com>
2022-03-01 11:34:20 -0500

    Update Distance_Pole-Nord.py

--- Distance_Pole-Nord.py
+++ Distance_Pole-Nord.py
@@ -1,5 +1,15 @@
-def dms_to_dd():
+from cmath import sqrt
 
-    return
+POLE_NORD = 86, 172
+PARIS = 48, 51, 23.81, 2, 21, 7.999
 
-def distance_
+def dms_to_dd(dms):
+    degv, minv, secv, degh, minh, sech = dms
+    return degv + minv/60 + secv/3600, degh + minh/60 + sech/3600
+
+def dist_dms_to_dd_pole_nord(dms):
+    verti, horiz = dms_to_dd(dms)
+    pnverti, pnhoriz = POLE_NORD
+    return sqrt((pnverti - verti)**2 + (pnhoriz - horiz)**2)
+
+print(dist_dms_to_dd_pole_nord(PARIS))
